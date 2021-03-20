9b59cafc868aff3af7a15fd6a0116e3e8fbf5f3e
recigio <recigio@gmail.com>
2021-03-20 17:56:44 -0300

    comunicacao com watson

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const WatsonAssistent = require('./ibm/watson')
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get('/', async (req, res) => {
+
+    const watson = new WatsonAssistent();
+
+    const htmlStart ="<html><body><div>";
+
+    const saida = "<p>"+ await watson.send(req.query.digite)+"</p>";
+
+    const form = "<form method='get'>" +
+        "<input type='text' name='digite' id='digite' placeholder='digite'>" +
+        "<input type='submit' name='enviar' id='enviar' value='Enviar!'" +
+        "</form>";
+
+    const htmlEnd="</div></body></html>";
+
+    res.send(htmlStart+saida+form+htmlEnd);
+
 })
 
 app.listen(port, () => {
---
+++ ibm/watson.js
@@ -0,0 +1,29 @@
+const AssistantV1 = require('ibm-watson/assistant/v1')
+const { IamAuthenticator } = require ('ibm-watson/auth');
+
+class WatsonAssistent {
+
+    constructor() {
+        this.assistant = new AssistantV1({
+            authenticator: new IamAuthenticator({ apikey: 'b13KLWlJ0SAqGvN103mpDWCSa0C8OHqL-KbkEySqaucb' }),
+            url: 'https://gateway.watsonplatform.net/assistant/api/',
+            version: '2018-02-16',
+        });
+    }
+
+    async send(inputtext){
+
+        const response =  await this.assistant.message(
+            {
+                workspaceId: '5b31e9cd-9851-432f-b0ee-2900f001022f',
+                input: { text: inputtext }
+            }
+        );
+
+        return response.result.output.text;
+
+    }
+
+}
+
+module.exports = WatsonAssistent;
