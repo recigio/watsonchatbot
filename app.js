8d71b1df03cc6775c5de2c9c5501dcea6e90ae6c
recigio <recigio@gmail.com>
2021-04-08 23:31:56 -0300

    adicionado icone

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ let started = false;
 app.listen(port, async () => {
     watson = new WatsonAssistent();
     await watson.createSession();
-    started = true;
     console.log(`Servidor está pronto! http://localhost:${port}`)
 })
 
@@ -20,33 +19,50 @@ app.get('/',cors(), async (req, res) => {
 
     try {
 
-        if(started){
+        const resultado = await watson.send(req.query.digite);
 
-            const covidApi  = new CovidApi();
-            const resultado = await watson.send(req.query.digite);
+        let saida = '';
 
-            let saida = '';
+        if(resultado.result.output.generic[0].title){
+            saida = resultado.result.output.generic[0].title;
 
-            if(resultado.result.output.generic[0].title){
-                saida = resultado.result.output.generic[0].title;
-
-                saida +='<ul>';
-                for(const option of resultado.result.output.generic[0].options){
-                    saida += "<li>"+option.label+"</li>";
-                }
-                saida +='<ul>';
-
-            }else {
-                saida = resultado.result.output.generic[0].text;
+            saida +='<ul>';
+            for(const option of resultado.result.output.generic[0].options){
+                saida += "<li>"+option.label+"</li>";
             }
+            saida +='<ul>';
 
-
-            res.send(saida);
+        }else {
+            saida = resultado.result.output.generic[0].text;
         }
 
+        res.send(saida);
+
     } catch (e){
-        console.log(e);
+
+        //cria nova sessao
+        //gambiarra por que ibm perde sessao as vezes
+        await watson.createSession();
+
+        const resultado = await watson.send(req.query.digite);
+
+        let saida = '';
+
+        if(resultado.result.output.generic[0].title){
+            saida = resultado.result.output.generic[0].title;
+
+            saida +='<ul>';
+            for(const option of resultado.result.output.generic[0].options){
+                saida += "<li>"+option.label+"</li>";
+            }
+            saida +='<ul>';
+
+        }else {
+            saida = resultado.result.output.generic[0].text;
+        }
+
+        res.send(saida);
     }
 
 
-})
+});
